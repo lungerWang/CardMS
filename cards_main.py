91c85ba28073c19e8f1f7042640f310433b53425
Allen <836414757@qq.com>
2018-12-18 14:40:05 +0800

    define operation method

--- cards_main.py
+++ cards_main.py
@@ -6,8 +6,18 @@ while True:
     action_str = input("Please select operation:")
     print("You select 【%s】" % action_str)
     if action_str in ["1", "2", "3"]:
+        if action_str == "1":
 
-        pass
+            cards_tools.new_card()
+
+        elif action_str == "2":
+
+            cards_tools.all_card()
+
+        elif action_str == "3":
+
+            cards_tools.search_card()
+
     elif action_str == "0":
         print("Goodbye!")
         break
--- cards_tools.py
+++ cards_tools.py
@@ -1,5 +1,5 @@
 def show_selection():
-    print("*" * 20)
+    print("*" * 50)
     print("Welcome to 【CardSystem】 V1.0")
     print("")
     print("1.Add card")
@@ -7,4 +7,21 @@ def show_selection():
     print("3.Search card")
     print("")
     print("0.Exit")
-    print("*" * 20)
+    print("*" * 50)
+
+
+def new_card():
+    print("-" * 50)
+    print("new card")
+
+
+def all_card():
+    print("-" * 50)
+    print("all card")
+
+
+def search_card():
+    print("-" * 50)
+    print("search card")
+
+
